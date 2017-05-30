6ee1ac1b8372bce8560c006c2ee36e36d3a7a6ae
Pickausaname <utroranneesr@gmail.com>
2017-05-30 11:49:26 +0300

    update

--- 4/4.py
+++ 4/4.py
@@ -4,3 +4,4 @@ arcpy.CheckOutExtension("Spatial")
 rasterlist = arcpy.ListRasters( )
 print arcpy.Describe(rasterlist[0]).dataType
 arcpy.RasterToPoint_conversion(r'C:\Users\S1mple\Desktop\Paper_14\4\Elevation','point.shp')
+#arcpy.GetCellValue_management,["SHAPE@XY"]
